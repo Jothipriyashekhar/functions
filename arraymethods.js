9f30ecbabf0238d41a7663211033c4f1978c87c7
jothipriya <jotitan27@gmail.com>
2016-12-19 11:54:20 +0530

    firt commit

---
+++ arraymethods.js
@@ -0,0 +1,23 @@
+/*array fuctions push,pop,shift and unshift*/
+
+var fruits=["orange","apple","banana","grapes"];
+//pulls the last element of the array
+fruits.pop();
+console.log(fruits);
+
+var vegetables=["carrot","tomato"];
+//append the elements at the end of the array
+vegetables.push("potato");
+console.log(vegetables);
+
+var animals=["cat","rat","dog"];
+//pulls the first element of the array
+animals.shift();
+console.log(animals);
+
+
+var birds=["parrot","pigeon","hen"];
+//prepend the elements at the beginning of the array
+birds.unshift("crow");
+console.log(birds);
+
